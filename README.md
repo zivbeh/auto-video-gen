f0b7bce145c8cff045466b5b9530da4cbbc7fe08
zivbeh <zivbeh@gmail.com>
2023-05-22 10:34:57 -0700

    initial

---
+++ README.md
@@ -0,0 +1,6 @@
+Automatic Video Generator
+
+Generates a video based on a prompt by adding images, videos, audios from the interet. Uses AI applications such as ChatGPT to create the script based on the prompt
+
+
+The prompt can be a the topic of the video, I will use chat gpt to generate a script which I will use to create voiceover and captions, next I will use chat gpt to generate a list of images and videos that can be on the screen like a slideshow as the voiceover is speaking. Finally, I would add all of it together and save it on a mp4 file.
---
+++ src/writer.py
@@ -0,0 +1,25 @@
+from moviepy.editor import ImageSequenceClip, AudioFileClip
+
+frame_rate = 24
+# Load images
+# ['image1.jpg', 'image2.jpg', 'image3.jpg']
+lst = []
+for i in range(1, frame_rate*3):
+    lst.append('./resources/images/image1.jpg')
+for i in range(1, frame_rate*2):
+    lst.append('./resources/images/image2.jpg')
+for i in range(1, frame_rate*1):
+    lst.append('./resources/images/image3.jpg')
+image_sequence = ImageSequenceClip(lst, frame_rate)
+
+# Load voiceover audio
+voiceover = AudioFileClip('./resources/audio/audio.mp3')
+print(image_sequence.duration)
+# Set the audio to match the duration of the image sequence
+voiceover = voiceover.set_duration(image_sequence.duration)
+
+# Combine images and voiceover
+video = image_sequence.set_audio(voiceover)
+
+# Export the video as an MP4 file
+video.write_videofile('./output/output.mp4', codec='mpeg4', audio_codec='aac')
