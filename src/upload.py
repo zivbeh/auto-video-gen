272a222b470b24c4c288a21660d1c55305737a06
zivbeh <63635842+zivbeh@users.noreply.github.com>
2023-09-27 07:59:35 -0700

    Update upload.py

--- src/upload.py
+++ src/upload.py
@@ -1,4 +1,8 @@
-#!/usr/bin/python
+# This code is a Python script for uploading a video to YouTube using the YouTube Data API.
+# It defines functions for
+# handling authentication,
+# initializing the upload,
+# and implementing resumable uploads.
 
 import httplib2
 import os
@@ -54,29 +58,32 @@ https://developers.google.com/api-client-library/python/guide/aaa_client_secrets
 """ % os.path.abspath(os.path.join(os.path.dirname(__file__),
                                    CLIENT_SECRETS_FILE))
 
+# Define valid privacy statuses for uploaded videos
 VALID_PRIVACY_STATUSES = ("public", "private", "unlisted")
 
-
+# Function to get an authenticated service for accessing YouTube API
 def get_authenticated_service():
+    # Create an OAuth flow from client secrets file
     flow = flow_from_clientsecrets(CLIENT_SECRETS_FILE,
                                    scope=YOUTUBE_UPLOAD_SCOPE,
                                    message=MISSING_CLIENT_SECRETS_MESSAGE)
-
+    # Create or load credentials and authorize the HTTP client
     storage = Storage("%s-oauth2.json" % sys.argv[0])
     credentials = storage.get()
 
     if credentials is None or credentials.invalid:
         credentials = run_flow(flow, storage)
-
+    # Build and return the YouTube API service
     return build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
                  http=credentials.authorize(httplib2.Http()))
 
-
+# Function to initialize the video upload
 def initialize_upload(youtube, args):
     tags = None
     if args["keywords"]:
         tags = args["keywords"].split(",")
 
+    # Create a dictionary representing the video metadata and status
     body = dict(
         snippet=dict(
             title=args["title"],
@@ -109,34 +116,38 @@ def resumable_upload(insert_request):
     retry = 0
     while response is None:
         try:
-            print("Uploading file...")
-            status, response = insert_request.next_chunk()
+            print("Uploading file...")  # Print a message to indicate the upload is in progress
+            status, response = insert_request.next_chunk()  # Attempt to upload the next chunk of the video
+
             if response is not None:
                 if 'id' in response:
-                    print("Video id '%s' was successfully uploaded." % response['id'])
+                    print("Video id '%s' was successfully uploaded." % response['id'])  # If a video ID is present in the response, the upload was successful
                 else:
-                    exit("The upload failed with an unexpected response: %s" % response)
+                    exit("The upload failed with an unexpected response: %s" % response)  # If there is no video ID, exit the program with an error message
+
         except HttpError as e:
             if e.resp.status in RETRIABLE_STATUS_CODES:
-                error = "A retriable HTTP error %d occurred:\n%s" % (e.resp.status, e.content)
+                error = "A retriable HTTP error %d occurred:\n%s" % (e.resp.status, e.content)  # Handle retriable HTTP errors
             else:
-                raise
+                raise  # Raise other HTTP errors
+
         except RETRIABLE_EXCEPTIONS as e:
-            error = "A retriable error occurred: %s" % e
+            error = "A retriable error occurred: %s" % e  # Handle other retriable errors
 
         if error is not None:
-            print(error)
-            retry += 1
+            print(error)  # Print the error message
+            retry += 1  # Increment the retry counter
             if retry > MAX_RETRIES:
-                exit("No longer attempting to retry.")
+                exit("No longer attempting to retry.")  # If the maximum number of retries is exceeded, exit the program
 
             max_sleep = 2 ** retry
             sleep_seconds = random.random() * max_sleep
-            print("Sleeping %f seconds and then retrying..." % sleep_seconds)
+            print("Sleeping %f seconds and then retrying..." % sleep_seconds)  # Sleep for a random duration and then retry
             time.sleep(sleep_seconds)
 
 
 if __name__ == '__main__':
+    # Define video upload parameters
     args = {
         "file": "/Users/zivbe/Documents/automatic-video-gen/src/output.mp4",
         "title": "California",
@@ -146,11 +157,15 @@ if __name__ == '__main__':
         "privacyStatus": "private"
     }
 
+    # Check if the specified file exists
     if not os.path.exists(args["file"]):
-        exit("Please specify a valid file.")
+        exit("Please specify a valid file.")  # If the file does not exist, exit with an error message
 
+    # Get an authenticated YouTube service
     youtube = get_authenticated_service()
+
     try:
+        # Initialize the video upload
         initialize_upload(youtube, args)
     except HttpError as e:
         print("An HTTP error %d occurred:\n%s" % (e.resp.status, e.content))
